070c47a540885cd984c323054d9ffaf6ff791831
thejunni <juniarta633@gmail.com>
2023-04-14 13:35:54 +0800

    file migration Kampus

---
+++ database/migrations/2023_04_14_035451_create_kampuses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kampuses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('mahasiswa_id');
+            $table->foreign('mahasiswa_id')->references('id')->on('mahasiswas')->onDelete('cascade');
+            $table->string('nama_kampus');
+            $table->string('alamat_kampus');
+            $table->string('provinsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kampuses');
+    }
+};
